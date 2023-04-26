fef7538ad14df9d2245305f1c3a3f37abeb017d4
Asus <Bichk_di78@student.itstep.org>
2023-04-26 13:36:19 +0300

    lesson4

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-#Напишіть функцію squares_list(lst), яка приймає
-#список lst чисел та повертає новий список зі
-#значеннями, які є квадратами елементів вхідного списку.
-#Напишіть код, що демонструє роботу функції та показує
-#список з квадратами елементів у деяких списках.
-import random
-lst = [random.randint(1 , 20) for i in range(5)]
-print("Било:" , *lst)
-def squares_list(lst):
-    return [i ** 2 for i in lst]
-print('Стало:', *squares_list(lst))
+#Задано словник зі словами та їх значеннями. Напишіть
+#функцію long_words(dictionary), яка приймає цей
+#словник та повертає список всіх слів, що містять не менше
+#ніж 5 символів (не забудьте оператор return). Напишіть
+#код, що демонструє роботу функції та показує список слів
+#у деяких словниках.
+def long_words(dictionary):
+   return [finish for finish in dictionary if len(finish) >= 5]
+slovo = {'Aplle' : 'Frut' , 'Yellow' : 'Color' , 'Marking' : 'Vegetable '}
+print(long_words(slovo))
